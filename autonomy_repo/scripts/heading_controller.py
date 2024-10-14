aefe25aef621f8eb8e6d4bd4f77dc6e34640d960
RogerDAI1217 <tdaiaa@connect.ust.hk>
2024-10-14 10:52:22 -0700

    foo

--- autonomy_repo/scripts/heading_controller.py
+++ autonomy_repo/scripts/heading_controller.py
@@ -10,16 +10,20 @@ from asl_tb3_msgs.msg import TurtleBotControl,TurtleBotState
 
 class HeadingController(BaseHeadingController):
     def __init__(self) -> None:
-        super().__init__("HeadingController")
-        self.kp=2.0
-	
+        super().__init__("heading_controller")
+        self.declare_parameter("kp",2.0)
+        #self.kp=2.0
+    
+    @property
+    def kp(self)->float:
+        return self.get_parameter("kp").value
+ 
     def compute_control_with_goal(self,state: TurtleBotState,goal: TurtleBotState) -> TurtleBotControl:
-    	print("here")
-    	heading_err=wrap_angle(goal.theta-state.theta)
-    	angular_velocity=self.kp*heading_err
-    	control_msg = TurtleBotControl()
-    	control_msg.omega=angular_velocity
-    	return control_msg
+        heading_err=wrap_angle(goal.theta-state.theta)
+        angular_velocity=self.kp*heading_err
+        control_msg = TurtleBotControl()
+        control_msg.omega=angular_velocity
+        return control_msg
 
 
 if __name__ == "__main__":
